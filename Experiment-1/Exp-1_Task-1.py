1d084f4db3a5c3330d4a26bf86425845f63a68e0
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-30 11:52:40 +0530

    Add files via upload

---
+++ Experiment-1/Exp-1_Task-1.py
@@ -0,0 +1,37 @@
+#EXPERIMENT-1
+#TASK-1: Write a Program, to print any ASCII Art from the website http://www.chris.com/ascii/
+
+print ("""
+    _.---._                                                _.--'  `-.
+   /       `--.                                         .-'          \
+
+  /            `-.      \                    /       .-'              |
+  |               `\     \                  /      .'                 |
+  |                 `\    \                /     .'                   |
+  `               .mmm`\   \              /    ./mmmmm                '
+  \             .mmmmmmm`\  \            /    / mmmmmmmm             /
+   \           .mm-mm/mmm.`\ \          /   ./ mmm\mm/mmmmm         /
+    \         mmmmmm\mmmmm  \ `\       /   / / mmmm/mmmmmm         /
+     \       .mmmmmmm\mmm` m\m\ \     /   /m/   mm/mmmmmmm.       /
+      \    .mmmmmmmmmm``   mm\m\ \   /   /m/mmm    mmmmmmmmmmm   /
+       \   mmmmmmmmm`    -mmmm\m\ \ /   /m/mmm-mm    mmmmmmmm'  /
+        `\ mm``/mmm.    mmm.-_m\m\ /-\ /m/mm-mmmm     mmm      /
+          \    mmm'     `mmmmm.-\m\{__}m/-mmmmm             _./
+           `-.________     `mmmmm \[ ]  mmmmm    ___.---::.'
+            ..::'.'. .`---.__ mmmm/  \ mmm ___.-'. . .`. .`:.
+           /: . . . . .`. .  `--' .__'  --' . . . . . . . . .\
+
+          .. . .                . . _  .                 `   .
+          |  '                     \_ .                      |
+          `                       .|_|                       '
+           \                       |/\.                     /
+            \                    ./\| \.                   /
+             \                   / ||  \                  /
+              `.                /  ||   \               ./
+                `\            ./   ||:F_P:.           ./
+                  `-._      ./     ||      `-._____.-'
+                      `----'       / \
+
+                                   |_/
+
+""")
---
+++ Experiment-1/Exp-1_Task-3.py
@@ -0,0 +1,4 @@
+//EXPERIMNET-1
+//TASK-3: Write a Personal Greeter Program.
+name = input("What's your name?")
+print("Hello", name)
