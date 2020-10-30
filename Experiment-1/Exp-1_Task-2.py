fd70724638a36a7a08f31ce1e0875e07a7b6281c
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-30 12:12:53 +0530

    Update Exp-1_Task-2.py

--- Experiment-1/Exp-1_Task-2.py
+++ Experiment-1/Exp-1_Task-2.py
@@ -1,3 +1,3 @@
-//EXPERIMENT-1
-//TASK-2: Write a Program to print (in the middle of the screen) your Name, UID and Section as listed below using only one print statement
+#EXPERIMENT-1
+#TASK-2: Write a Program to print (in the middle of the screen) your Name, UID and Section as listed below using only one print statement
 print ("\t\t\t\t\tAARYA RAJOJU\n\t\t\t\t\t20BCS7669\n\t\t\t\t\t39A")
