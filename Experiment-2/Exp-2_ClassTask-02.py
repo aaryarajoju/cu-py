b483332ef1b0f4f2a655e86285831677d81eea25
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-31 12:48:22 +0530

    Create Exp-2_ClassTask-02.py

---
+++ Experiment-2/Exp-2_ClassTask-02.py
@@ -0,0 +1,10 @@
+#Class Task-2
+#Password Program
+# Password
+# Demonstrates the if statement
+print("Welcome to System Security Inc.")
+print("-- where security is our middle name\n")
+password = input("Enter your password: ")
+if password == "secret":
+ print("Access Granted")
+input("\n\nPress the enter key to exit.")
