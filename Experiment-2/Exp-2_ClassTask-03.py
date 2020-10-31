b9487de349a2f6d78bc5e9c547901fe5ee0ba99b
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-31 12:47:52 +0530

    Create Exp-2_ClassTask-03.py

---
+++ Experiment-2/Exp-2_ClassTask-03.py
@@ -0,0 +1,12 @@
+#Class Task-3
+#Granted or Denied Program
+# Granted or Denied
+# Demonstrates an else clause
+print("Welcome to System Security Inc.")
+print("-- where security is our middle name\n")
+password = input("Enter your password: ")
+if password == "secret":
+ print("Access Granted")
+else:
+ print("Access Denied")
+input("\n\nPress the enter key to exit.")
