360b468e8a2bd710a53f09ed2f156349a1d77279
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-31 11:33:37 +0530

    Create Exp-1_ClassTask-3.py

---
+++ Experiment-1/Exp-1_ClassTask-3.py
@@ -0,0 +1,20 @@
+#Class Task-3
+#Program 'Fancy Credits"
+
+# Fancy Credits
+# Demonstrates escape sequences
+
+print("\t\t\tFancy Credits")
+
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+print("\t\t\t\tby")
+print("\t\t\tMichael Dawson")
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+
+print("\nSpecial thanks goes out to:")
+print("My hair stylist, Henry \'The Great,\' who never says
+\"can\'t.\"")
+
+#sound the system bell
+print("\a")
+input("\n\nPress the enter key to exit.")
