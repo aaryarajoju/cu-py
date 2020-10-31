416c9a1a067dfda07b83326e4d635bc29ea561c9
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-31 12:46:08 +0530

    Create Exp-2_ClassTask-05.py

---
+++ Experiment-2/Exp-2_ClassTask-05.py
@@ -0,0 +1,16 @@
+#Class Task-5
+#Three Year Old
+
+# Three Year-Old Simulator
+# Demonstrates the while loop
+
+print("\tWelcome to the 'Three-Year-Old Simulator'\n")
+print("This program simulates a conversation with a three-year-old
+child.")
+print("Try to stop the madness.\n")
+response = ""
+while response != "Because.":
+ response = input("Why?\n")
+print("Oh. Okay.")
+
+input("\n\nPress the enter key to exit.")
