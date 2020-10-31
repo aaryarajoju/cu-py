d0d0b81377cd3effa688b4b8a00924d81b237cbe
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-31 11:31:01 +0530

    Create Exp-1_ClassTask-1(2).py

---
+++ Experiment-1/Exp-1_ClassTask-1(2).py
@@ -0,0 +1,12 @@
+#Class Task-1
+#WAP to print a Quote, press Enter to display the name of the Author of the Quote, and finally press Enter to exit. 
+#(Hint: Use two print and input statements)
+
+#print quote in desired form
+print("\n Health is Wealth")
+
+#print author name desired form
+print("\n Anonymous")
+
+#wait for user to exit
+input("\n\nPress the enter key to exit.")
