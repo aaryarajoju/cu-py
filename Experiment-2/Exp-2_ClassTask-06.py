6eb6c7e8a275fd18849595cd5b699659a3f1e09d
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-31 12:44:22 +0530

    Create Exp-2_ClassTask-06.py

---
+++ Experiment-2/Exp-2_ClassTask-06.py
@@ -0,0 +1,12 @@
+#Class Task-6
+#Loopy String
+
+# Loopy String
+# Demonstrates the for loop with a string
+
+word = input("Enter a word: ")
+print("\nHere's each letter in your word:")
+for let in word:
+ print(let)
+ 
+input("\n\nPress the enter key to exit.")
