fe78a2dfca9a6f74b5a311d839536efb2a690a2e
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-31 11:29:59 +0530

    Create Exp-1_ClassTask-1(1).py

---
+++ Experiment-1/Exp-1_ClassTask-1(1).py
@@ -0,0 +1,11 @@
+#Class Task-1
+#WAP to print favorite best flavor of your ice cream and wait for the user to exit.
+
+#take user input
+d = input("\n\n Enter your favorite ice cream flavor \n\n")
+
+#print data in desired form
+print("\n\nYou like",d," flavor the most in ice creams ")
+
+#wait for user to exit
+input("\n\nPress the enter key to exit.")
