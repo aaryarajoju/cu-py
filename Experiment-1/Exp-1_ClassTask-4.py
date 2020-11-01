a99973a5eca29cfd6c7d156c7a96b6ac78b5738f
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-01 13:52:41 +0530

    Update Exp-1_ClassTask-4.py

--- Experiment-1/Exp-1_ClassTask-4.py
+++ Experiment-1/Exp-1_ClassTask-4.py
@@ -6,7 +6,7 @@
 
 print("You can concatenate two " + "strings with the '+' operator.")
 print("\nThis string " + "may not " + "seem terr" + "ibly impressive." \
- + "But what " + "you don't know" + " is that\n" + "it's onereal" \
+ + "But what " + "you don't know" + " is that\n" + "it's one real" \
  + "l" + "y" + " long string, created from the concatenation " \
  + "of " + "twenty-two\n" + "different strings, broken across " \
  + "six lines." + " Now are you" + " impressed? " + "Okay,\n" \
