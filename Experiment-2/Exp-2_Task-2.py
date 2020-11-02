db519e752fe0159f0574a357545ee59ab691ea9d
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-03 00:55:43 +0530

    Update Exp-2_Task-2.py

--- Experiment-2/Exp-2_Task-2.py
+++ Experiment-2/Exp-2_Task-2.py
@@ -1,5 +1,9 @@
 # EXPERIMENT - 2
 # TASK - 2 : Write a Python function to find the Max of three numbers
 
+def maximum(x,y,z):
+    return max(x,y,z)
+
+#main
 x, y, z = input("Enter a three value: ").split()
-print(max(x,y,z))
+print(maximum(x,y,z))
