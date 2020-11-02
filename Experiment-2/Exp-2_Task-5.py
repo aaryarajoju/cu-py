27425ef820f0ff779a87574f87586c01ceb52f99
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-03 01:33:35 +0530

    Update Exp-2_Task-5.py

--- Experiment-2/Exp-2_Task-5.py
+++ Experiment-2/Exp-2_Task-5.py
@@ -1,2 +1,27 @@
 # EXPERIMENT - 2
 # TASK - 5 : Create a program wages.py that assumes people are paid double time for hours over 60. They get paid for at most 20 hours overtime at 1.5 times the normal rate.
+
+#wages.py
+
+import random
+
+def printSalary(hours, wage):
+    if hours < 40:
+        print("The salary cannot be generated")
+    elif hours >= 40 :
+        print(salary(hours, wage))
+
+def salary(hours, wage):
+    if hours == 40:
+        salary = 40 * wage
+    elif hours > 40 and hours < 60:
+        salary = 40 * wage + (hours-40)*(1.5*wage)
+    elif hours > 60:
+        salary = 40 * wage + 20*(1.5*wage) + (hours-60)*(2*wage)
+    return salary
+
+#main
+numOfHours  = random.randint(35,75)
+regularWage = int(input("Enter the regular wage: "))
+print("The number of hours are:", numOfHours)
+printSalary(numOfHours, regularWage)
