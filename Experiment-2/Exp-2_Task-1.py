e20559029980baadfc38fd3daaa50abc47be5774
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-03 00:21:02 +0530

    Create Exp-2_Task-1.py

---
+++ Experiment-2/Exp-2_Task-1.py
@@ -0,0 +1,11 @@
+# EXPERIMENT - 1
+#TASK - 1 : WAP to take word input from user and display the input in jumbled form
+
+import random
+
+word = input ("Enter a word: ")
+word = list(word)
+random.shuffle(word)
+word = "".join(word)
+
+print(word)
