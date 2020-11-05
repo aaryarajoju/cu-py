fd4848e1b74e6e6c6056be358801e8c1ba7c117d
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-05 22:06:36 +0530

    Update Exp-2_Task-5 (simplified).py

--- Experiment-2/Exp-2_Task-5 (simplified).py
+++ Experiment-2/Exp-2_Task-5 (simplified).py
@@ -1,4 +1,4 @@
-  # EXPERIMENT - 2
+# EXPERIMENT - 2
 # TASK - 5 : Create a program wages.py that assumes people are paid double time for hours over 60. They get paid for at most 20 hours overtime at 1.5 times the normal rate.
 
 #wages.py
